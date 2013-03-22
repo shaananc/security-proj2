1a6e349f1899244822d55978c804435482b48dca
Shaanan Cohney <shaananc@gmail.com>
2013-03-22 18:51:34 -0400

    added nodeinfo

---
+++ upenn-cis553/penn-search/NodeInfo.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   NodeInfo.h
+ * Author: user
+ *
+ * Created on March 22, 2013, 3:23 AM
+ */
+
+#ifndef NODEINFO_H
+#define	NODEINFO_H
+
+using namespace ns3;
+
+typedef struct NodeInfo {
+    int32_t location;
+    Ipv4Address address;
+} NodeInfo;
+
+
+
+#endif	/* NODEINFO_H */
+

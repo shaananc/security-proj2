63e73723eebc8d4f93c4bede4c64ed5dbde3cc34
Shaanan Cohney <shaananc@gmail.com>
2013-04-17 00:45:41 -0400

    string conversion fixes

--- upenn-cis553/execute.sh
+++ upenn-cis553/execute.sh
@@ -1,2 +1,3 @@
-gdb --args ../build/debug/upenn-cis553/simulator-main --inet-topo=topologies/small.topo --routing=ns3 --scenario=scenarios/custom2.sce
+#gdb --args ../build/debug/upenn-cis553/simulator-main --inet-topo=topologies/small.topo --routing=ns3 --scenario=scenarios/custom2.sce
+../build/debug/upenn-cis553/simulator-main --inet-topo=topologies/small.topo --routing=ns3 --scenario=scenarios/custom2.sce
 
